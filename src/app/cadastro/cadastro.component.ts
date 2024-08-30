9092bcbfefb7383621dee274abd8daa819da657d
Marcio Pancheri <mjpancheri@gmail.com>
2024-08-30 20:48:49 -0300

    add fill out address when cep is valid

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { ConsultaCepService } from "../services/consulta-cep.service";
 
 @Component({
   selector: "app-cadastro",
@@ -8,7 +9,7 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./cadastro.component.css"],
 })
 export class CadastroComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private cepService: ConsultaCepService) {}
 
   ngOnInit(): void {}
 
@@ -18,5 +19,31 @@ export class CadastroComponent implements OnInit {
     } else {
       alert("Formulário inválido");
     }
+  }
+
+  consultarCep(event: any, form: NgForm) {
+    const cep = event.target.value.replace(/\D/g, "");
+
+    if (cep.length !== 8) {
+      return;
+    }
+
+    this.cepService.getConsultaCep(cep).subscribe((data: any) => {
+      if (data.erro) {
+        event.target.nextElementSibling.innerText = "CEP inesistente!";
+        return;
+      }
+      this.popularEndereco(form, data);
+    });
+  }
+
+  popularEndereco(form: NgForm, data: any) {
+    form.form.patchValue({
+      endereco: data.logradouro,
+      complemento: data.complemento,
+      bairro: data.bairro,
+      cidade: data.localidade,
+      estado: data.uf,
+    });
   }
 }
